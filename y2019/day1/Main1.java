8299b9af6f5fbcab4585277e9c4b34662a0daed0
Illio Suardi <illio.suardi@gmail.com>
2020-10-20 14:01:51 +0800

    Change packaging structure

---
+++ y2019/day1/Main1.java
@@ -0,0 +1,37 @@
+package y2019.day1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Optional;
+
+class Main1 {
+
+    static int getFuel(int amount) {
+        return Optional.of(Math.floorDiv(amount, 3) - 2).map(i -> i >= 0 ? i : 0).get();
+    }
+
+    static int getTotalFuel(int amount) {
+        int fuel = amount;
+        int total = 0;
+        while (fuel > 0) {
+            fuel = getFuel(fuel);
+            total += fuel;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        String filePath = "y2019/day1/input.txt";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            reader.lines()
+                        .map(Integer::parseInt)
+                        .map(Main1::getFuel)         // comment out to see value for part two
+                        .map(Main1::getTotalFuel)    // comment out to see value for part one
+                        .reduce(Integer::sum)
+                        .ifPresent(System.out::println);
+        } catch (FileNotFoundException ignored) { }
+    }
+
+}
---
+++ y2019/day2/Main2.java
@@ -0,0 +1,5 @@
+package y2019.day2;
+
+public class Main2 {
+
+}
