97ed61169d7376f867e67e777069f3e22d042fc5
Illio Suardi <illio.suardi@gmail.com>
2020-12-03 04:52:41 +0800

    Tidy up day2.jl

--- y2020/day2/day2.jl
+++ y2020/day2/day2.jl
@@ -1,43 +1,30 @@
-function partone()
-    total = 0
+function main()
+    total1 = 0
+    total2 = 0
     io = open("y2020/day2/input.in")
     for line in eachline(io)
         m = match(r"(?<min>.+)-(?<max>.+) (?<char>.+): (?<password>.+)", line)
         count = 0
         c = m[ :char][1]
-        for char in m[ :password]
+        str = m[ :password]
+        min = parse(Int64, m[ :min])
+        max = parse(Int64, m[ :max])
+        for char in str
             if c == char
                 count += 1
             end
         end
-        if count >= parse(Int64, m[ :min]) && count <= parse(Int64, m[ :max])
-            total += 1
+        if count >= min && count <= max
+            total1 += 1
+        end
+        if xor(str[min] == c, str[max] == c)
+            total2 += 1
         end
     end
     close(io)
-    println(total)
+    println(total1)
+    println(total2)
     return nothing
-end
-
-function parttwo()
-    total = 0
-    io = open("y2020/day2/input.in")
-    for line in eachline(io)
-        m = match(r"(?<min>.+)-(?<max>.+) (?<char>.+): (?<password>.+)", line)
-        c = m[ :char][1]
-        if xor(m[ :password][parse(Int64, m[ :min])] == c,
-            m[ :password][parse(Int64, m[ :max])] == c)
-            total += 1
-        end
-    end
-    close(io)
-    println(total)
-    return nothing
-end
-
-function main()
-    partone()
-    parttwo()
 end
 
 main()
