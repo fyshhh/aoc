fd0ebba43c3024f0c65e0a9e8aea19fe12a4dfff
Illio Suardi <illio.suardi@gmail.com>
2020-12-13 20:22:11 +0800

    Add y2020/day13

---
+++ y2020/day13/day13.jl
@@ -0,0 +1,30 @@
+function parseinput()::Pair{Int, Vector{String}}
+    return readlines("y2020/day13/input.in") |> a -> parse(Int, a[1]) => (split(a[2], ","))
+end
+
+function partone(pair::Pair{Int, Vector{String}})
+    (filter(!=("x"), pair.second) .|> s -> parse(Int, s)) |> v -> mapreduce(i -> i => i - (pair.first % i), (p, q) -> p.second < q.second ? p : q, v) |> p -> println(p.first * p.second)
+end
+
+function parttwo(pair::Pair{Int, Vector{String}})
+    dict = Dict{Int, Int}()              # dict describing congruences
+    for (i, s) in enumerate(pair.second)
+        if s != "x"
+            dict[i - 1] = parse(Int, s)
+        end
+    end
+    sum = 0
+    prod = reduce(*, values(dict))
+    for i in keys(dict)
+        println(gcdx(div(prod, dict[i]), dict[i]))
+        println(gcdx(dict[i], div(prod, dict[i])))
+        sum += i * div(prod, dict[i]) * gcdx(div(prod, dict[i]), dict[i])[2]
+    end
+    # interestingly, this is necessary as Julia's gcdx produces minimal Bezout coefficients that
+    # somehow cause the sum to be negated
+    mod1(-sum, prod) |> println
+end
+
+pair = parseinput()
+partone(pair)
+parttwo(pair)
